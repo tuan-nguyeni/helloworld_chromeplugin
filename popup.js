40db7d9befb817f215f38f39a7d4debc89815638
tuan <tuan.nguyen@datics-consulting.com>
2023-08-26 11:15:13 +0200

    first commit

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const helloButton = document.getElementById('helloButton');
+    helloButton.addEventListener('click', function() {
+        alert('Hello World!');
+    });
+});
